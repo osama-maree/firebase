a13c1d28fed4f74944c16f8cbe3a8b0c4eb8d4d3
Osama Maree <108696087+osama-maree@users.noreply.github.com>
2023-02-06 19:56:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";//use react-router-dom for single page application
 import ForgetPassword from "./component/forgetPassword/ForgetPassword";
 import Home from "./component/home/Home";
 import Login from "./component/login/Login";
 import Signup from "./component/signup/Signup";
 import Update from "./component/updateProfile/Update";
 import AuthProvider from "./context/AuthContext";
-import { RequireAuth } from "./context/RequieredAuth";
+import { RequireAuth } from "./context/RequieredAuth";//to protect home page and if user not logged in not allow show this page
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
-          <AuthProvider>
+          <AuthProvider> 
             <Routes>
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />} />
@@ -27,7 +27,7 @@ function App() {
               <Route
                 path="/"
                 element={
-                  <RequireAuth>
+                  <RequireAuth>   //protect this routes
                     <Home />
                   </RequireAuth>
                 }
