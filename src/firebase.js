f9a3599348d7997b60f676a90de6fa0edb73b80e
Osama Maree <108696087+osama-maree@users.noreply.github.com>
2023-02-06 20:02:46 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgaP1E5X-ielAWYGp0w4a4lzSLP0pAweU",
-  authDomain: "authproject-8ff06.firebaseapp.com",
-  projectId: "authproject-8ff06",
-  storageBucket: "authproject-8ff06.appspot.com",
-  messagingSenderId: "390061065228",
-  appId: "1:390061065228:web:e25dbea33845b41b461a29",
-  measurementId: "G-GBRNP03NTS",
+const firebaseConfig = {//use .env to protect my information
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEGEING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
